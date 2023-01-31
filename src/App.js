081290529a12c07d17aa1802144b8af4ed023033
Szymon Roszyk <roszyk.szymon@gmail.com>
2023-01-31 21:01:08 +0100

    stoper start

--- src/App.js
+++ src/App.js
@@ -6,23 +6,33 @@ class App extends React.Component {
     super();
 
     this.state = {
-      seconds: 0
+      seconds: 0,
+      decySeconds: 0
     };
 
-    setInterval(() => { this.tick() }, 1000);
+    setInterval(() => { this.tick() }, 100);
   }
 
   tick() {
     this.setState((state, props) => {
+      let decySeconds = state.decySeconds + 1;
+      let seconds = state.seconds;
+
+      if (decySeconds === 10) {
+        seconds++;
+        decySeconds = 0;
+      }
+
       return {
-        seconds: state.seconds + 1
+        decySeconds: decySeconds,
+        seconds: seconds
       };
     });
   }
 
   render() {
     return (
-      <div>{this.state.seconds}</div>
+      <div>{this.state.seconds} : {this.state.decySeconds}</div>
     );
   }
 }
