8a132bf98ef7431dc1afeec371f6d75b0df2f9b4
Szymon Roszyk <roszyk.szymon@gmail.com>
2023-02-01 18:31:18 +0100

    Stop stoper

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  intervalId;
+
   constructor() {
     super();
 
     this.state = {
       seconds: 0,
-      decySeconds: 0
+      decySeconds: 0,
+      isActive: false
     };
 
     // Jezeli startStoper ma być funkcja obslugujaca event to
@@ -15,6 +18,7 @@ class App extends React.Component {
     // tak aby funkcja w momencie wywolania "wiedziala" czym jest "this"
     // this - to oczywiscie obiekt naszego komponentu
     this.startStoper = this.startStoper.bind(this);
+    this.stopStoper = this.stopStoper.bind(this);
   }
 
   tick() {
@@ -34,16 +38,35 @@ class App extends React.Component {
     });
   }
 
-  startStoper(){
-    setInterval(() => { this.tick() }, 100);
+  startStoper() {
+    if (!this.isActive) {
+      this.intervalId = setInterval(() => { this.tick() }, 100);
+      this.setState((state, props)=>{
+        return {
+          ...state,
+          isActive: true
+        }
+      });
+    }
+  }
+
+  stopStoper() {
+    clearInterval(this.intervalId);
+    this.setState((state, props)=>{
+      return {
+        ...state,
+        isActive: false
+      }
+    });
   }
 
   render() {
     return (
-    <div>
-      <button onClick={this.startStoper}>Start</button>
-      <div>{this.state.seconds} : {this.state.decySeconds}</div>
-    </div>
+      <div>
+        {!this.state.isActive && <button onClick={this.startStoper}>Start</button>}
+        {this.state.isActive && <button onClick={this.stopStoper}>Stop</button>}
+        <div>{this.state.seconds} : {this.state.decySeconds}</div>
+      </div>
     );
   }
 }
